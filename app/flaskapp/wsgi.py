f046db55411a104d26aa8a800084e9be176fc14c
ykato0525 <81909162+ykato0525@users.noreply.github.com>
2022-04-12 11:33:54 +0900

    キャッシュのdelete

--- app/flaskapp/wsgi.py
+++
@@ -1,7 +0,0 @@
-from tokenize import Triple
-from app import app
-
-if __name__ == '__main__':
-    # debug要らなくなったら、debug=Trueを消してください
-    # portの指定を追加(0207)
-    app.run(host='0.0.0.0', port=5000, debug=True)

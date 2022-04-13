a26ffa9c61b897a872fbecdf117f9ab2089e820b
ykato0525 <81909162+ykato0525@users.noreply.github.com>
2022-04-13 10:04:22 +0900

    M1用にファイルの書き換え

--- frontend/pages/blog/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next';
-import { useRouter } from 'next/router';
-const Id: NextPage = () => {
-    const router = useRouter();
-    return (
-        <div>ブログ</div>
-    );
-};
-export default Id;
